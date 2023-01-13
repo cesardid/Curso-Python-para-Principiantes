769ce723757fa96aecf49c9fa755b6c571b4b0bd
Cesar Chaparro <chaparrosogamosocesarivan@gmail.com>
2023-01-12 22:13:36 -0500

    Típos de datos básicos
    Se muestran los diferentes típos de datos básicos en Python, agregando como comentario el nombre de cada tipo de dato.

---
+++ datatype.py
@@ -0,0 +1,30 @@
+
+# Strings
+print("Hello world")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+
+
+# Integer
+
+print (10)
+
+# Float
+
+print (30.1)
+
+# Boolean
+
+False
+True 
+
+# List
+
+
+
+
+
+
+
+
